641a7b5be3feb228470780881446fcce2cd67a08
U-WARRENCE\Admin <quynhnhu170218@gmail.com>
2024-04-06 22:03:07 +0700

    them tep js vao

---
+++ bai3/bai3.js
@@ -0,0 +1,34 @@
+console.log('hello WORLD FROM JVS')
+//it is dynamic 
+let c = 5 ;
+let d=7.8;
+let x = 16 + "Vilvo";
+
+//jvs string 
+let carName= "Mercedes";
+let streetName='NguyenThiTan la 1 con duong';
+//always use ` ` -> it works for every thinglet streetName='NguyenThiTan la 1 con duong';
+let streetName2=`NguyenThiTan "''"la 1 con duong`;
+
+//numbers 
+//how to know if it is a number or not 
+let f = 5 ;
+let e=7.2;
+
+//boolean
+let a =4; 
+let b = 3 
+// (b==a) //false 
+
+//undefined 
+let car; 
+
+//constant 
+//gia tri k doi theo thoi gian
+const value= 5; 
+// value=8; 
+console.log(`hello WORLD FROM JVS`, value )
+//console bao loi vi const k the bi thay doi gia tri
+
+console.log ("test string", `x=` a, b, c, d, x);
+//success
