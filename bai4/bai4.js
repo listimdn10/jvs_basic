da9a1f8ef0bde2f344480ebf2c084b32e59f30a7
U-WARRENCE\Admin <quynhnhu170218@gmail.com>
2024-04-06 22:32:17 +0700

    them tep js 4 vao

---
+++ bai4/bai4.js
@@ -0,0 +1,13 @@
+let a = `I'm MỘT STRING
+ - CHUỖI`;                   
+console.log(a);
+console.log( 4 + 5 + 'a'); // in ra 9a 
+console.log ('a' + 5 + 4); // in ra a54 
+// vi th1 jvs se hieu la ta dang cong 1 so voi 1 chuoi = 4+5+a=9+a
+//th2 la no cong 1 chuoi voi so = a5 + 4 = a54
+
+//string length 
+let text = `we love "Mercedes" cars `; 
+console.log(text.length) //7 
+
+
